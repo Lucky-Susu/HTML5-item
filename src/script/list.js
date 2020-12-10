5d7fe2f0905461ac9d0fefc47e6b97b2a40e5ebb
Lucky-Susu <1375056101@qq.com>
2020-12-10 11:42:15 +0800

    first commit

---
+++ src/script/list.js
@@ -0,0 +1,22 @@
+! function($) {
+    const $list = $('.list ul');
+    //1.渲染list.html页面
+    $.ajax({
+        url: 'http://localhost:8080/dashboard/month02/projectname/php/listdata.php',
+        dataType: 'json'
+    }).done(function(data) {
+        let $strhtml = '';
+        $.each(data, function(index, value) {
+            $strhtml += `
+                <li>
+                    <a href="detail.html?sid=${value.sid}">
+                        <img src="${value.url}"/>
+                        <p>${value.title}</p>
+                        <span>￥${value.price}</span>
+                    </a>
+                </li>
+            `;
+        });
+        $list.html($strhtml);
+    });
+}(jQuery);
